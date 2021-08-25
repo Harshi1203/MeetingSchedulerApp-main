428c582c23f295cb75d256bd00825aec0ae42a05
Harshitha <harshitha3377@gmail.com>
2021-08-25 16:53:53 +0530

    first commit

---
+++ public/myschedule.js
@@ -0,0 +1,46 @@
+
+
+
+    //   ,
+    // headerToolbar: {
+    //   center: 'addEventButton'
+    // },
+    // customButtons: {
+    //   addEventButton: {
+    //     text: 'add event...',
+    //     click: function() {
+    //       var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+    //       var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+    //
+    //       if (!isNaN(date.valueOf())) { // valid?
+    //         calendar.addEvent({
+    //           title: 'dynamic event',
+    //           start: date,
+    //           allDay: true
+    //         });
+    //         alert('Great. Now, update your database...');
+    //       } else {
+    //         alert('Invalid date.');
+    //       }
+    //     }
+    //   }
+    // }
+// });
+//
+//     calendar.on('dateClick', function(info) {
+//        var title = prompt("title of the event: ");
+//        var date = new Date(info.dateStr + 'T00:00:00');
+//        if (!isNaN(date.valueOf())) { //valid?
+//                calendar.addEvent({
+//                  title: title,
+//                  start: date,
+//                  allDay: true
+//                });
+//                alert('Great. Now, update your database...');
+//              } else {
+//                alert('Invalid date.');
+//              }
+//     });
+//
+//     calendar.render();
+//   });
